3a86b810e05387355be8d3e4f1f83889857a077c
Ehsan Mokhtari <ehsanm94@gmail.com>
2016-11-17 10:56:21 +0330

    add error 404 controller and view

---
+++ app/Controller/PageNotFound.php
@@ -0,0 +1,23 @@
+<?php
+namespace App\Controller;
+
+use \Zardak\Template;
+
+class PageNotFound
+{
+    public static function index()
+    {
+
+    	$views_chain = array(
+            '404',
+            'base' => array(
+                'title' => 'صفحه یافت نشد!',
+            ),
+        );
+        $tpl = new Template($views_chain);
+        $tpl->render();
+
+        header("HTTP/1.0 404 Not Found");
+        exit();
+    }
+}
