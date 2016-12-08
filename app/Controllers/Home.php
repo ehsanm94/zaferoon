09885c278ea3704ba7b6e3d98cce8dfe3d5fc7f5
Ehsan Mokhtari <ehsanm94@gmail.com>
2016-12-08 14:08:57 +0330

    rename Controller directory to Controllers

---
+++ app/Controllers/Home.php
@@ -0,0 +1,23 @@
+<?php
+namespace App\Controllers;
+
+use Zardak\Controller;
+use Zardak\Template;
+
+class Home extends Controller
+{
+    public function index() {
+        $views_chain = array(
+            'home',
+            'base' => array(
+                'title' => 'خانه',
+                'resources' => array(
+                    'style/css/home.css',
+                    'js/home.js',
+                ),
+            ),
+        );
+        $tpl = new Template($views_chain);
+        $tpl->render();
+    }
+}
